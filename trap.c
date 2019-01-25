a72efd5955f6c614a18d681fa00a1b0b5f37a578
dzy <daizy15@mails.tsinghua.edu.cn>
2019-01-25 15:12:32 +0800

    benchmark: timer interval

--- trap.c
+++ trap.c
@@ -7,6 +7,7 @@
 #include "x86.h"
 #include "traps.h"
 #include "spinlock.h"
+#include "rdtsc.h"
 
 // Interrupt descriptor table (shared by all CPUs).
 struct gatedesc idt[256];
@@ -49,6 +50,7 @@ trap(struct trapframe *tf)
   switch(tf->trapno){
   case T_IRQ0 + IRQ_TIMER:
     if(cpuid() == 0){
+			SINGLE_REPORT;
       acquire(&tickslock);
       ticks++;
       wakeup(&ticks);
